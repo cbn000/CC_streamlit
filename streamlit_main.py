2240f2c3126d20e9d81d5dc21f71ecf90f8569a7
Johannes-Schall <jschall84@gmail.com>
2023-10-11 11:13:23 +0200

    cleaned up streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -14,19 +14,11 @@ def load_pickles():
     encoder_dict = pickle.load(open("cc_label_encoders.pkl", "rb"))
     return model, encoder_dict
 
-
-
-st.title('Title')
-
-st.header('Problem Description')
-st.write('Description')
-
-st.header('Prediction')
-st.write('Description')
-
-# Load data
-cc_data = pd.read_csv("data/cc_approvals.data", header=None)
-column_names = ["Gender",
+# Function for loading the data
+@st.cache_data
+def load_data(data_file_path):
+    cc_data = pd.read_csv(data_file_path, header=None)
+    column_names = ["Gender",
                 "Age",
                 "Debt",
                 "Married",
@@ -42,57 +34,86 @@ column_names = ["Gender",
                 "ZipCode",
                 "Income",
                 "ApprovalStatus"]
-# Add column names
-cc_data.columns = column_names
-#define mask to filter only for values we are interested in
-mask = ['Gender', 'Age', 'Debt', 'Married', 'BankCustomer', 'YearsEmployed', 'PriorDefault', 'Employed', 'CreditScore',
-        'DriversLicense', 'Citizen', 'Income'] 
-# apply mask
-X = cc_data[mask]
-y = cc_data["ApprovalStatus"]
-# Replace '?' with NaN and drop NaN
-X = X.replace('?', np.NaN)
-X = X.dropna(axis=0)
-y = y[X.index]
-# converting "Total Charges" to numeric
-X["Age"] = X["Age"].astype(float)
+    # Add column names
+    cc_data.columns = column_names
+    return cc_data
 
-data = X.iloc[0, :].to_dict()
+# function for preprocessing the data
+def preprocess_data(dataframe):
+    mask = ['Gender', 'Age', 'Debt', 'Married', 'BankCustomer', 'YearsEmployed', 'PriorDefault', 'Employed', 'CreditScore',
+        'DriversLicense', 'Citizen', 'Income'] 
+    # apply mask
+    X = dataframe[mask]
+    y = dataframe["ApprovalStatus"]
+    # Replace '?' with NaN and drop NaN
+    X = X.replace('?', np.NaN)
+    X = X.dropna(axis=0)
+    y = y[X.index]
+    # converting "Total Charges" to numeric
+    X["Age"] = X["Age"].astype(float)
+    return X, y
+
+# function for applying the encoder
+def apply_encoder(dataframe, encoder_dict):
+    for col in dataframe.columns:
+        if col in list(encoder_dict.keys()):
+            column_encoder = encoder_dict[col]
+            dataframe.loc[:, col] = column_encoder.transform(dataframe.loc[:, col])
+        else:
+            continue
+    return dataframe
+
+# Load data
+cc_data = load_data("data/cc_approvals.data")
+# Preprocess data
+X, y = preprocess_data(cc_data)
+# Load model and encoder
+model, encoder_dict = load_pickles()
+
+
+# Body of the app
+st.title('Title')
+
+st.header('Problem Description')
+st.write('Description')
+
+st.header('Prediction')
+st.write('Description')
+
+# Creating the input fields using the data of the first row of the train data as default values
+data_row0 = X.iloc[0, :].to_dict()
 # The dtypes
 dtypes = X.dtypes.to_dict()
 # max and min values for the numerical columns
 max_values = X.max().to_dict()
 min_values = X.min().to_dict()
-
-# Creating the input fields using the data of the first row of the train data as default values
+# Generate input fields in loop
 input_fields = {}
 for col in X.columns:
     if dtypes[col] == "int64":
         input_fields[col] = st.slider(
-            col, min_value=min_values[col], max_value=max_values[col], value=data[col]
+            col, min_value=min_values[col], max_value=max_values[col], value=data_row0[col]
         )
     elif dtypes[col] == "float64":
         input_fields[col] = st.slider(
-            col, min_value=min_values[col], max_value=max_values[col], value=data[col]
+            col, min_value=min_values[col], max_value=max_values[col], value=data_row0[col]
         )
     else:
         input_fields[col] = st.selectbox(col, X[col].unique())
 
+# Inputs to dataframe
 df_input = pd.DataFrame([input_fields])
-st.write(df_input)
-
-# Load model and encoder
-model, encoder_dict = load_pickles()
+# st.write(df_input)
 
 # Apply encoder to input data
-for col in df_input.columns:
-    if col in list(encoder_dict.keys()):
-        column_le = encoder_dict[col]
-        df_input.loc[:, col] = column_le.transform(df_input.loc[:, col])
-    else:
-        continue
+df_input = apply_encoder(df_input, encoder_dict)
 
 # Make prediction
 prediction = model.predict(df_input)
 
-st.write(prediction)
+#st.write(prediction)
+# Building a indicator for the prediction
+if prediction == 1:
+    st.success('Approve')
+else:
+    st.error('Reject!')
